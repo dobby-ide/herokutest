210358abbda43b56df4f476c3a129e1955813ac7
Fabio Privitera <fabio_privitera@yahoo.it>
2022-04-07 11:27:21 +0300

    index.js: working with db in heroku

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const port = process.env.PORT || 8080;
 const app = express();
-
+var mysql = require('sql');
 const db = [{ name: 'tiina' }, { name: 'jack' }];
-
+var dbConnection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DB,
+  multipleStatements: true,
+});
 app.get('/names', (req, res) => {
   res.send(db);
+});
+
+var connection = mysql.createConnection(dbConnection);
+app.get('/', (req, res) => {
+  connection.query('SELECT * from location', (error, results) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.send(results);
+    }
+  });
 });
 
 const server = app.listen(port, () => {
